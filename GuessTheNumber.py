cf9e13fb898270862c3a524b759a69519466b78c
PrajwalGowda29 <prajwal.brb.24.beai@acharya.ac.in>
2025-05-26 19:24:02 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,13 +1,30 @@
-#guessthenumber
 import random
-print("PRAJWAL BR, USN: 1AY24AI083, SEC: O")
-num = int(input("Enter the number between 1 to 9: "))
-while num < 1 or num > 9:
-    num = int(input("Invalid input! Please enter a number between 1 to 9: "))
-print(num, "is the number entered by user")
-comp_num = random.randint(1, 9)
-print(comp_num, "is the number randomly taken by computer")
-if num == comp_num:
-    print("User guess is Absolutely Correct!!!!!!!!!!")
-else:
-    print("Oh NO! User guess is Incorrect!!!!!!!!!!")
+print("Prajwal BR \nUSN:1AY24AI083 \nSec:O")
+def guess_the_number():
+    print("Welcome to Guess the Number!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    max_attempts = 10
+    
+    while attempts < max_attempts:
+        try:
+            # Get player's guess
+            guess = int(input("Take a guess: "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts!")
+                return
+        
+        except ValueError:
+            print("Please enter a valid number.")
+    
+    print(f"Game over! The number was {secret_number}.")
+
+guess_the_number()
